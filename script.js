d7c1159a5a57864d13880e1398bd97e72c5fada1
Miguel Ángel García <92162464+mikaeltevet@users.noreply.github.com>
2023-06-01 10:10:03 -0600

    fixes part 9

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 const display = document.querySelector('.display')
 const buttons = Array.from(document.querySelectorAll('.buttons button'))
-const specialChars = new Set(['%', '*', '÷', '−', '+', '='])
+const operators = new Set(['÷', '×', '−', '+'])
 let output = '0'
 
 function calculate(btnValue) {
-  switch (btnValue) {
-    case '÷':
-      output += '/'
-      break
-    case '×':
-      output += '*'
-      break
-    case '−':
-      output += '-'
-      break
-    case '=':
-      if (output) {
-        output = eval(output.replace('%', '/100')).toString()
-      }
-      break
-    case 'AC':
-      output = '0'
-      break
-    case 'DEL':
-      output = output.slice(0, -1)
-      if (output === '') output = '0'
-      break
-    default:
-      if (output === '0' && !specialChars.has(btnValue)) {
-        output = btnValue
-      } else if (!output && specialChars.has(btnValue)) {
-        return
-      } else {
-        output += btnValue
-      }
+  const lastChar = output[output.length - 1]
+
+  if (operators.has(btnValue)) {
+    if (operators.has(lastChar)) return
+    else output += btnValue
+  } else {
+    switch (btnValue) {
+      case '=':
+        if (output && !operators.has(lastChar)) {
+          output = eval(
+            output
+              .replace('%', '/100')
+              .replace('÷', '/')
+              .replace('×', '*')
+              .replace('−', '-')
+          ).toString()
+        }
+        break
+      case 'AC':
+        output = '0'
+        break
+      case 'DEL':
+        output = output.slice(0, -1)
+        if (output === '') output = '0'
+        break
+      default:
+        if (output === '0' && !operators.has(btnValue)) {
+          output = btnValue
+        } else if (!output && operators.has(btnValue)) {
+          return
+        } else {
+          output += btnValue
+        }
+    }
   }
   display.value = formatForDisplay(output)
 }
