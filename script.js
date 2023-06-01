62b80774f9d4c0092e125f69e26ceffc26418bb5
Miguel Ángel García <92162464+mikaeltevet@users.noreply.github.com>
2023-05-31 23:56:48 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.querySelector('.display')
+const buttons = Array.from(document.querySelectorAll('.button'))
+const specialChars = new Set(['%', '*', '/', '-', '+', '='])
+let output = ''
+
+function calculate(btnValue) {
+  switch (btnValue) {
+    case '=':
+      if (output) {
+        output = eval(output.replace('%', '/100'))
+      }
+      break
+    case 'AC':
+      output = ''
+      break
+    case 'DEL':
+      output = output.slice(0, -1)
+      break
+    default:
+      if (!output && specialChars.has(btnValue)) return
+      output += btnValue
+  }
+  display.value = output
+}
+
+buttons.forEach((button) =>
+  button.addEventListener('click', (event) =>
+    calculate(event.target.dataset.value)
+  )
+)
